b8f96ee132d83da47ddb8913a1de470391eb9bbc
NurKevser <nurkevserkadiroglu@gmail.com>
2021-07-24 18:32:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+let restart = document.getElementById("restart");
+
+function jump() {
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
+
+let checkDead = setInterval(function(){
+    let characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = 
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("u lose.")
+    }
+},10);
+
+restart.addEventListener("click", function(){
+    location.reload();
+});
+
